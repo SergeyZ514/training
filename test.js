81193052421b6d2bd61ebdef0ac152374d3eba36
Sergey Zimonov <zimonov87@bk.ru>
2022-02-05 19:27:37 +0600

    изменения каталога

--- test.js
+++ test.js
@@ -1,14 +1,22 @@
-let animal = {
-  eat() {
-    console.log("Eat");
-  },
-};
+function loadScript(src, callback) {
+  let script = document.createElement("script");
+  script.src = src;
+  script.onload = () => callback(script);
+  document.head.append(script);
+}
 
-let rabbit = {
-  __proto__: animal,
-  eat: function () {
-    super.eat();
+/* loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js",
+  (script) => {
+    alert(`Здорово, скрипт ${script.src} загрузился`);
+    alert(_); // функция, объявленная в загруженном скрипте
   },
-};
+); */
 
-rabbit.eat();
+loadScript("/my/script.js", function (script) {
+  alert(`Здорово, скрипт ${script.src} загрузился, загрузим ещё один`);
+
+  loadScript("/my/script2.js", function (script) {
+    alert(`Здорово, второй скрипт загрузился`);
+  });
+});
